0ecc4e698ee083dad5eba21604c5ffed7a2886d2
dbuck087 <dbuck087@gmail.com>
2023-04-28 23:40:14 +0000

    worked on bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "FoodTracker/app/javascript/stylesheets/application.scss"
+import "/app/javascript/stylesheets/application.scss"
 
 Rails.start()
 Turbolinks.start()
